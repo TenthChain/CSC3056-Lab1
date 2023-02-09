b4b29ee334edf1144ad23585fe365fd72ef6cea4
Sam Stuart <sam@tcdevworld.com>
2023-02-09 04:14:26 +0000

    Add UserTest logic

--- src/tests/UserTest.java
+++ src/tests/UserTest.java
@@ -1,9 +1,16 @@
 package tests;
 
+import model.User;
+
 public class UserTest {
 	
 	public static void main(String[] args) {
 		
+		// To test this we will declare a new object of type User
+		// with values that we can test our object with
+		User testUser = new User("mike", "my_passwd", "Mike", "Smith", "07771234567");
+		
+		System.out.println(testUser);
+		
 	}
-	
 }
